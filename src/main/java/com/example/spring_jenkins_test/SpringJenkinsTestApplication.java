fa7fec6efb9c284bb873a144ee3f825810396ecb
zkffhtm6523 <zkffhtm6523@gmail.com>
2021-12-31 15:24:58 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_jenkins_test/SpringJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jenkins_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_jenkins_test/SpringJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jenkins_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
